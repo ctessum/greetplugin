e65ed990b3ddb525482e504fce14ebc5fa0dcc16
ddieffen <ddieffen@gmail.com>
2015-01-23 15:19:10 -0600

    Updated crawler to include displaced sources
    Also fixed the TraceMix method and add results of the mix to the fake
    output

--- Buisness/Entities/POutput.cs
+++ Buisness/Entities/POutput.cs
@@ -17,6 +17,9 @@ namespace Greet.Plugins.SplitContributions.Buisness.Entities
         int _resourceID;
         Results _results;
         bool _isDisplaced = false;
+        List<double> displacementRatios;
+        List<Guid> _displacedVertices;
+        List<Guid> _displacedOutputs;
         #endregion
 
         #region public accessors
@@ -25,6 +28,7 @@ namespace Greet.Plugins.SplitContributions.Buisness.Entities
             get { return _isDisplaced; }
             set { _isDisplaced = value; }
         }
+
         public Results Results
         {
             get { return _results; }
@@ -36,15 +40,35 @@ namespace Greet.Plugins.SplitContributions.Buisness.Entities
             get { return _resourceID; }
             set { _resourceID = value; }
         }
+
         public Guid Id
         {
             get { return _id; }
             set { _id = value; }
         }
+
         public Value Quantity
         {
             get { return _quantity; }
             set { _quantity = value; }
+        }
+
+        public List<double> DisplacementRatios
+        {
+            get { return displacementRatios; }
+            set { displacementRatios = value; }
+        }
+
+        public List<Guid> DisplacedVertices
+        {
+            get { return _displacedVertices; }
+            set { _displacedVertices = value; }
+        }
+
+        public List<Guid> DisplacedOutputs
+        {
+            get { return _displacedOutputs; }
+            set { _displacedOutputs = value; }
         }
         #endregion
     }
